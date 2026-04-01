5ee32cddba9b33edb339641a2957704fca1fdd67
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-04-01 12:02:51 -0700

    Adding some comments to clarify config setup and fixing weird formatting

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ let sql;
 // connecting to the db 
 const db = new sqlite3.Database('./src/database/database.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) console.error(err.message);
-    else console.log('Successfully connected to database!');
+    else console.log('Connected to database!');
 });
 
 module.exports = db;
--- src/config/passport-config.js
+++ src/config/passport-config.js
@@ -4,30 +4,43 @@ var crypto = require('crypto');
 const db = require('./db')
 
 
-passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, cb) {
-    db.get('SELECT * FROM users WHERE email = ?', [ email ], function(err, row) {
-    if (err) {return cb(err); }
-    if (!row) {return cb(null, false, {message: 'Incorrect email or password.'}); }
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, cb) { // usernameField lets passport know we're not using username to log in/reg
+  db.get('SELECT * FROM users WHERE email = ?', [ email ], function(err, row) {
+    if (err) {
+      return cb(err); 
+    }
+    if (!row) {
+      return cb(null, false, {message: 'Incorrect email or password.'}); 
+    }
 
     const hashedPassword = Buffer.from(row.password_hash, 'hex');
     const salt = Buffer.from(row.salt, 'hex');  
 
     crypto.pbkdf2(password, salt, 310000, 32, 'sha256', function(err, supplied) {
-        if (err) { return cb(err); }
-        console.log('stored hash:', row.password_hash);
-        if (!crypto.timingSafeEqual(hashedPassword, supplied)) {
-            return cb(null, false, { message: 'Incorrect email or password.' });
-        }
+      if (err) {
+        return cb(err); 
+      }
+
+      if (!crypto.timingSafeEqual(hashedPassword, supplied)) {
+        return cb(null, false, { message: 'Incorrect email or password.' });
+      }
+      
       return cb(null, row);
+
     });
   });
 }));
 
-passport.serializeUser((user, cb) => cb(null, user.userId));
+passport.serializeUser((user, cb) => {
+  //console.log('serializing user:', user); // debug print
+  cb(null, user.userId);
+});
 
 passport.deserializeUser(function(id, cb) {
-    db.get('SELECT * FROM users WHERE userId = ?', [id], (err, user) => cb(err, user));
-  
+  db.get('SELECT * FROM users WHERE userId = ?', [id], (err, user) => {
+    //console.log('deserializing user:', user); // debug print  
+    cb(err, user);
+  });
 });
 
 module.exports = passport;
