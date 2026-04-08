6f0416e02e75216701b0c56a697a266a73ffd1e4
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-04-08 13:15:58 -0700

    adding in /user route to automatically direct to profiles

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,10 +4,9 @@ const { isAuthenticated } = require('../middleware/isAuth');
 
 // gets the user page
 // routes according to user type
-router.get('/', isAuthenticated, (req, res, next) => {
-  
-  console.log("User role: " + req.user.role);
-  let role = req.user.role;
+router.get('/user', isAuthenticated, (req, res, next) => {
+
+  let role = req.user.Role;
   if (role === 'admin')
   {
     res.redirect('/admin/dashboard');
@@ -23,6 +22,28 @@ router.get('/', isAuthenticated, (req, res, next) => {
   else // defaults to customer dash
   {
     res.redirect('/customer/dashboard');
+  }
+  
+});
+
+router.get('/user-profile', isAuthenticated, (req, res, next) => {
+
+  let role = req.user.Role;
+  if (role === 'admin')
+  {
+    res.redirect('/admin/profile');
+  }
+  else if (role === 'vendor')
+  {
+    res.redirect('/vendor/profile');
+  }
+  else if (role === 'driver')
+  {
+    res.redirect('/driver/profile');
+  }
+  else 
+  {
+    res.redirect('/customer/profile');
   }
   
 });
