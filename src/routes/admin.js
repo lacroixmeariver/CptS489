cc259ffbdf17de1f5c49e1f4fc33d7195029f121
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-04-08 13:13:00 -0700

    adding in admin base routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -8,5 +8,19 @@ router.get('/dashboard', isAuthenticated, (req, res) => {
     res.render('admin/admin-dashboard', { user: req.user });
 });
 
+router.get('/manage-users', isAuthenticated, (req, res) => {
+    res.render('admin/manage-user', { user: req.user })
+});
 
+router.get('/issues', isAuthenticated, (req, res) => {
+    res.render('admin/disputes', { user: req.user })
+});
+
+router.get('/revenue', isAuthenticated, (req, res) => {
+    res.render('admin/revenue', { user: req.user })
+});
+
+router.get('/profile', isAuthenticated, (req, res) => {
+    res.render('profile', { user: req.user })
+});
 module.exports = router;
