51a5ad6075bf0e09af36616cf8ca5a3d6a67c9ee
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-04-10 14:06:22 -0700

    Removing unecessary modules

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
-const multer  = require('multer')
-const path = require('path');
-const storage = multer.diskStorage({
-  destination: 'uploads/',
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname); 
-    cb(null, req.body.email + '-vendor' + ext); // formatting file name 
-  }
-});
-const upload = multer({ storage });
 const userModel = require('../models/user');
 const createUser = require('../middleware/createUser');
 const redirectByRole = require('../middleware/redirectByRole');
@@ -19,7 +9,7 @@ const redirectByRole = require('../middleware/redirectByRole');
 // if user already logged in it'll re-route to dashboard
 router.get('/login', function(req, res, next) {
   if (req.user) {
-    redirectByRole.dashRedirect(res, user.role);
+    redirectByRole.dashRedirect(res, user.Role);
     return res.redirect('/customer/dashboard');
   }
   res.render('auth/login', { title: 'Login'});
@@ -28,20 +18,14 @@ router.get('/login', function(req, res, next) {
 
 // post info to login page 
 router.post('/login', function(req, res, next) {
-   
-    passport.authenticate('local', function(err, user, info) {
-        if (err) {
-            return next(err)
-        };
-        if (!user) {
-            return res.redirect('/login')
-        };
-        req.logIn(user, function(err) {
-            if (err) {
-                return next(err)
-            }
-            redirectByRole.dashRedirect(res, user.role); 
-        });
+  passport.authenticate('local', function(err, user, info) {
+    if (err) return next(err);
+    if (!user) return res.redirect('/login');
+    console.log('user.Role:', user.Role);
+    req.logIn(user, function(err) {
+      if (err) return next(err);
+      redirectByRole.dashRedirect(res, user.Role); 
+    });
     })(req, res, next);
 });
 
@@ -49,37 +33,35 @@ router.post('/login', function(req, res, next) {
 // if user already logged in it'll re-route to dashboard
 router.get('/register', function(req, res, next) {
   if (req.user) {
-    redirectByRole.dashRedirect(res, user.role);
+    redirectByRole.dashRedirect(res, user.Role);
   }
   res.render('auth/register', { title: 'Register' });
 });
 
 function loginAndRedirect(req, res, next, userId) {
-    userModel.getUserByID(userId, (err, user) => {
-        console.log('getUserByID result:', err, user);
-        if (err) return next(err);
-        if (!user) return next(null, false, { message: 'Incorrect email or password.' });
-        req.login(user, (err) => {
-            if (err) return next(err);
-            res.redirect('/user');
-        })
-    });
-
+  userModel.getUserByID(userId, (err, user) => {
+    if (err) return next(err);
+    if (!user) return next(null, false, { message: 'Incorrect email or password.' });
+    req.login(user, (err) => {
+      if (err) return next(err);
+      res.redirect('/user');
+    })
+  });
 }
 
 // post info to the register page 
-router.post('/register', upload.array('documents', 5), (req, res, next) => {
-    const { email, password, firstName, lastName, phoneNumber, role } = req.body;
-   createUser.createUser({ email, password, firstName, lastName, phoneNumber, role }, (err, userID) => {
-        if (err) return next(err);
-        loginAndRedirect(req, res, next, userID)
-   })
+router.post('/register', (req, res, next) => {
+  const { email, password, firstName, lastName, phoneNumber, role } = req.body
+  createUser.createUser({ email, password, firstName, lastName, phoneNumber, role }, (err, userID) => {
+    if (err) return next(err);
+    loginAndRedirect(req, res, next, userID)
+  })
 })
 
 
 // get forgot password page
 router.get('/forgot-password', (req, res, next) =>{
-    res.render('auth/forgot-password', { title: 'Forgot Password' });
+  res.render('auth/forgot-password', { title: 'Forgot Password' });
 });
 
 // route to deserialize user and end session 
