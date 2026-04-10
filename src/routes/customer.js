0a5256167dbd8c7082ed2a4e86cd61cf4a3462e0
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-04-10 14:07:54 -0700

    Removing unecessary elements from register

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -8,46 +8,17 @@ router.get('/dashboard', isAuthenticated, (req, res) => {
     res.render('customer/customer-dashboard', { user: req.user });
 });
 
+
 // get the partial order status page element
 router.get('/order-status', isAuthenticated, (req, res) => {
-    db.all(`
-        SELECT o.*, m.MerchantName 
-        FROM Orders o
-        JOIN Customers c ON o.CustomerID = c.CustomerID
-        JOIN Merchants m ON o.MerchantID = m.MerchantID
-        WHERE c.UserID = ? AND o.OrderStatus NOT IN ('Completed', 'Cancelled')
-        ORDER BY o.TimeOrdered DESC
-    `, [req.user.userID], (err, orders) => {
-        res.render('partials/order-status', { orders: orders || [] });
-    });
 });
 
 // get the partial order history page element
 router.get('/order-history', isAuthenticated, (req, res) => {
-    db.all(`
-        SELECT o.*, m.MerchantName 
-        FROM Orders o
-        JOIN Customers c ON o.CustomerID = c.CustomerID
-        JOIN Merchants m ON o.MerchantID = m.MerchantID
-        WHERE c.UserID = ?
-        ORDER BY o.TimeOrdered DESC
-    `, [req.user.userID], (err, orders) => {
-        res.render('partials/order-history', { orders: orders || [] });
-    });
 });
 
 // get the partial reviews page element
 router.get('/reviews', isAuthenticated, (req, res) => {
-    db.all(`
-        SELECT r.*, m.MerchantName
-        FROM Reviews r
-        JOIN Customers c ON r.CustomerID = c.CustomerID
-        JOIN Merchants m ON r.MerchantID = m.MerchantID
-        WHERE c.UserID = ?
-        ORDER BY r.ReviewDate DESC
-    `, [req.user.userID], (err, reviews) => {
-        res.render('partials/customer-reviews', { reviews: reviews || [] });
-    });
 });
 
 
@@ -72,17 +43,6 @@ router.get('/cart', isAuthenticated, (req, res) => {
 router.get('/checkout', isAuthenticated, (req, res) => {
     res.render('customer/checkout', { user: req.user })
 });
-/*
-    TODO: CUSTOMER ROUTES
-    - BROWSE
-    - MY ORDERS 
-        - ORDER HISTORY PAGE NEEDS STANDALONE
-    - MY ACCOUNT
-    - CURRENT ORDER 
-    - ORDER HISTORY
-    - REVIEWS
 
-
-*/
 
 module.exports = router;
