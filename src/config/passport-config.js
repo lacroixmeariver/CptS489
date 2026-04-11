2e3ec93d8c2627e8966ac6968b34d79c491c8c10
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-04-10 17:27:25 -0700

    Some beginning elememts for the admin models

--- src/config/passport-config.js
+++ src/config/passport-config.js
@@ -1,43 +1,32 @@
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local');
 var crypto = require('crypto');
-var userModel = require('../models/user')
-// const db = require('./db')
+const db = require('./db')
 
 
 passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, cb) { // usernameField lets passport know we're not using username to log in/reg
-  console.log("EMAIL: ", email);
-  userModel.findUserByEmail(email, (err, user) => {
+  db.get('SELECT * FROM Users WHERE Email = ?', [ email ], function(err, row) {
     if (err) return cb(err);
-
-    if (!user) {
-      return cb(null, false, { message: 'Incorrect email or password.' });
-    }
-    const hashedPassword = Buffer.from(user.Password_hash, 'hex');
-    crypto.pbkdf2(password, user.Salt, 310000, 32, 'sha256', function(err, supplied) {
-      if (err) {
-        return cb(err); 
-      }
-      if (hashedPassword.length !== supplied.length || !crypto.timingSafeEqual(hashedPassword, supplied)) {
-        return cb(null, false, { message: 'Incorrect email or password.' });
-      }
-      return cb(null, user);
+    if (!row) return cb(null, false, {message: 'Incorrect email or password.'}); 
+    const hashedPassword = Buffer.from(row.Password_hash, 'hex');
+    crypto.pbkdf2(password, row.Salt, 310000, 32, 'sha256', function(err, supplied) {
+      if (err) return cb(err); 
+      if (!crypto.timingSafeEqual(hashedPassword, supplied)) return cb(null, false, { message: 'Incorrect email or password.' });
+      return cb(null, row);
     });
   });
-}))
+}));
 
 passport.serializeUser((user, cb) => {
-  // console.log('serializing user:', user); // debug print
+  //console.log('serializing user:', user); // debug print
   cb(null, user.UserID);
 });
 
-passport.deserializeUser((id, done) => {
-    userModel.getUserByID(id, (err, user) => {
-        if (err) return done(err);
-        if (!user) return done(null, false);
-        done(null, user);
-    });
+passport.deserializeUser(function(id, cb) {
+  db.get('SELECT * FROM Users WHERE UserID = ?', [id], (err, user) => {
+    //console.log('deserializing user:', user); // debug print  
+    cb(err, user);
+  });
 });
-
 
 module.exports = passport;
---
+++ src/models/admin.js
@@ -0,0 +1,48 @@
+const db = require('../config/db');
+
+/**
+ * TODO:
+ * need db queries for: 
+ * ban user/change status
+ * moderate content/remove posts 
+ * approve vendors 
+ * revenue data/info
+ * resolve disputes 
+ */
+
+function getVendorByID(MerchantID, cb){
+    db
+    .get('SELECT * FROM Merchants WHERE MerchantID = ?',
+        MerchantID, (err, row) => {
+        if (err) return cb(err);
+        cb(null, row);
+    });
+}
+
+function getDriverByID(DriverID, cb) {
+    db
+    .get('SELECT * FROM Drivers WHERE DriverID = ?',
+        DriverID, (err, row) => {
+        if (err) return cb(err);
+        cb(null, row);
+    });
+}
+
+// set vendor to valid 
+function setMerchantStatusToValid(MerchantID, cb){
+    db
+    .run(`UPDATE Merchants SET Verified = TRUE WHERE MerchantID = ?`, MerchantID);
+}
+
+// un-ban user
+function setMerchantStatusToValid(MerchantID, cb){
+    db
+    .run(`UPDATE Merchants SET Verified = TRUE WHERE MerchantID = ?`, MerchantID);
+}
+
+
+
+
+module.exports = {
+
+}
