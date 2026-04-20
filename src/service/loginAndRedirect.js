3d6aa63a8f67e71d88af2590b1ec82f48c67b554
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-04-19 20:20:57 -0700

    Adding some created utility functions

--- src/service/loginAndRedirect.js
+++ src/service/loginAndRedirect.js
@@ -1,16 +1,17 @@
-const userModel = require('../models/user');
+const userModel = require("../models/users");
 
 async function loginAndRedirect(req, res, next, userId) {
   try {
     const user = await userModel.getUserByID(userId);
-    if (!user) return next(new Error('Registration error, new user not found.'));
+    if (!user)
+      return next(new Error("Registration error, new user not found."));
     req.login(user, (err) => {
       if (err) return next(err);
-      res.redirect('/user');
+      res.redirect("/users");
     });
   } catch (err) {
     next(err);
   }
 }
 
-module.exports = { loginAndRedirect }
+module.exports = { loginAndRedirect };
